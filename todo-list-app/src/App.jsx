7a601809d0adfb900789998855a3bb2743bc77c4
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-09-12 11:57:44 +0330

    fix: fix some bugs

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { NewTodoForm } from "./NewTodoForm"
+import  NewTodoForm  from './NewTodoForm'
 import "./styles.css"
-import { TodoList } from "./TodoList"
+import  TodoList  from "./TodoList"
 
 export default function App() {
   const [todos, setTodos] = useState(() => {
